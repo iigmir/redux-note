ed273c6038ebbf724e4ea8062d669c2e77887ac9
iigmir <roc120j@gmail.com>
2020-02-04 12:09:07 +0800

    ASN

--- src/store/index.js
+++ src/store/index.js
@@ -4,5 +4,6 @@ import inputs from "../Inputs/store/store.js";
 export default createStore(
     // Reducers
     combineReducers({ inputs }),
+    // Redux devtool
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
