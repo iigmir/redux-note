b692d81d9d4b4ea9f001646a58748493da66b6a7
iigmir <roc120j@gmail.com>
2020-02-08 00:46:21 +0800

    Updates

---
+++ precache-manifest.e9e1894347134bd3e0c1c875745b438e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6db6843c04aaccf3f9bdc6d35bf9e571",
+    "url": "/redux-note/index.html"
+  },
+  {
+    "revision": "67fe77c8655509996e54",
+    "url": "/redux-note/static/css/main.c280e313.chunk.css"
+  },
+  {
+    "revision": "840d4d358273de16d1b0",
+    "url": "/redux-note/static/js/2.848a2e39.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/redux-note/static/js/2.848a2e39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67fe77c8655509996e54",
+    "url": "/redux-note/static/js/main.7bca6aca.chunk.js"
+  },
+  {
+    "revision": "6f8fcdee7452aad937b8",
+    "url": "/redux-note/static/js/runtime-main.ab03d8b6.js"
+  },
+  {
+    "revision": "c18405ab19877916b02048daf912b895",
+    "url": "/redux-note/static/media/logo.c18405ab.svg"
+  }
+]);
